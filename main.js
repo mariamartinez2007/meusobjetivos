f1c599ec5aff39b20feca20788a9bcad1c0c6eb2
mariamartinez2007 <159436788+mariamartinez2007@users.noreply.github.com>
2024-04-27 15:28:52 -0300

    o

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const bontoes = DocumentType.querySelectorAll(".botao");
 const textos  = DocumentType.querySelectorAll(".aba-conteudo");
+
+for(let i=0,i < bontoes.length,i++) {
+    botoes[i].onclick=function () {
+        for (let j =0,j< botoes.length,j++) {
