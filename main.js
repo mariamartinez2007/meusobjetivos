0cd88e244efc2a14d69884c738e86711f271455d
mariamartinez2007 <159436788+mariamartinez2007@users.noreply.github.com>
2024-04-29 09:28:54 -0300

    oo

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const bontoes = DocumentType.querySelectorAll(".botao");
 const textos  = DocumentType.querySelectorAll(".aba-conteudo");
 
-for(let i=0,i < bontoes.length,i++) {
+for(let i=0;i < bontoes.length;i++) {
     botoes[i].onclick=function () {
-        for (let j =0,j< botoes.length,j++) {
+
+        for (let j = 0;j< botoes.length;j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+            {
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+        {
+     {
+        const contadores=document.querySelectorAll(".contador");
+        const tempoObjetivo1=new Date("2023-10-05T00:00:00");
+        const tempoObjetivo2=new Date("2023-12-05T00:00:00");
+        const tempoObjetivo3=new Date("2023-12-30T00:00:00");
+        const tempoObjetivo4=new Date("2024-02-01T00:00:00"); 
+
+        const tempo=
+        [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+    function calcula Tempo(tempoObjetivo){
+        let tempoAtual
