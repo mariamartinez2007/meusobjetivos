cc6d356d2ab7b7e89ee7ca55467fe206eef87a36
mariamartinez2007 <159436788+mariamartinez2007@users.noreply.github.com>
2024-04-30 09:12:45 -0300

    000

--- main.js
+++ main.js
@@ -23,4 +23,34 @@ for(let i=0;i < bontoes.length;i++) {
         [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
     function calcula Tempo(tempoObjetivo){
-        let tempoAtual
+        let tempoAtual=new Date();
+        let tempoFinal=tempoObjetivo-tempoAtual;
+        let segundos=Math.floor(tempoFinal/1000);
+        let minutos=Math.floor(segundos/60);
+        let horas=Math.floor(minutos/60);
+        let dias=Math.floor(horas/24);
+
+        segundo %= 60;
+        minutos %=60;
+        horas %=24;
+        if(tempoFinal > 0){
+            return[dias,horas,minutos,segundos];
+        } else { 
+            return [0,0,0,0];
+        }
+    }
+    function atualizaCronometro(){
+        for (let i=0; i<contadores.length;i++){
+            document.getElementByld("dias"+i).textContent=calculeTempo(tempo[i])[0];
+            document.getElementByld("horas"+i).textContent=calculaTempo(tempos[i])[1];
+            document.getElementByld("min"+i).textContent=calculaTempo(tempos[i])[2];
+            document.getElementByld("seg"+i).textContent=calculaTempo(tempo[i])[3];
+        }
+    }
+
+    function comecaCronometro(){
+        atualizaCronometro();
+        setlnterval(atualizaCronometro,1000);
+
+        {
+            comecaCronometro();
